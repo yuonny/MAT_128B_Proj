124e96db8bc65aaa23a97af5a67a25ca797d1555
yuonny <dvkim@ucdavis.edu>
2025-05-02 13:37:21 -0700

    code

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
 season1 = ["HelpWanted", "ReefBlower", "Tea at the Treedome", "Bubblestand", "Ripped Pants"]
+
+with open('', 'w') as outfile:
+    for fname in season1:
+        path = "SpongeBob_SquarePants_Transcripts/"
+        fname = fname.replace(" ", "")
+        fname += ".txt"
+        
+        file_path = path + fname
+        with open(file_path) as infile:
+            for line in infile:
+                outfile.write(line)
