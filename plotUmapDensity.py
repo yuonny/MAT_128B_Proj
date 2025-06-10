cc53dbcab519e351e91217b92f14800d3424959f
yuonny <dvkim@ucdavis.edu>
2025-06-09 17:33:30 -0700

    calc highest density point

--- plotUmapDensity.py
+++ plotUmapDensity.py
@@ -1,5 +1,6 @@
 import pickle
 import numpy as np
+from numpy.linalg import norm
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
 import csv
@@ -10,10 +11,28 @@ from ot.sliced import sliced_wasserstein_distance
 older_seasons_umap = np.load("umap_old.npy")
 newer_seasons_umap = np.load("umap_new.npy")
 
-def density_output(reduced_vectors, window_title):
-    # this is how density is calculated 
+def calc_density(reduced_vectors):
     xy = np.vstack([reduced_vectors[:, 0], reduced_vectors[:, 1]])
     density = gaussian_kde(xy)(xy)
+    return density
+
+#finding which point has the highest density 
+def calc_max_density(density, reduced_vectors, token_file):
+    max_idx = np.argmax(density)
+    mode_point = reduced_vectors[max_idx]
+    
+    distances = norm(reduced_vectors - mode_point, axis=1)
+    index = np.argmin(distances)
+    
+    with open(token_file, "rb") as f:
+        tokenized = pickle.load(f)
+    return tokenized[index]
+
+
+def density_output(reduced_vectors, window_title):
+    # this is how density is calculated 
+    
+    density = calc_density(reduced_vectors)
 
     fig = plt.figure(figsize=(10, 7))
     fig.canvas.manager.set_window_title(window_title)
@@ -35,16 +54,18 @@ def plot_output(reduced_vectors):
     plt.tight_layout()
     
 def calc_wasserstein(old_script, new_script):
-
     sw_dist = sliced_wasserstein_distance(old_script, new_script,
                                          n_projections=100)
     return sw_dist
 
 print(calc_wasserstein(older_seasons_umap, newer_seasons_umap))
 
-plot_output(older_seasons_umap)
-plot_output(newer_seasons_umap)
-density_output(older_seasons_umap, "Older Seasons Transcript UMAP")
-density_output(newer_seasons_umap, "Newer Seasons Transcript UMAP")
+print(calc_max_density(calc_density(older_seasons_umap), older_seasons_umap,"tokensOld.pkl"))
+print(calc_max_density(calc_density(newer_seasons_umap),  newer_seasons_umap, "tokensNew.pkl"))
+
+# plot_output(older_seasons_umap)
+# plot_output(newer_seasons_umap)
+# density_output(older_seasons_umap, "Older Seasons Transcript UMAP")
+# density_output(newer_seasons_umap, "Newer Seasons Transcript UMAP")
 
 plt.show()
